f3f7e90031bc3744fcf425b09ebfdfe81a604de3
CaprarellaRiccaro <128700856+CaprarellaRiccaro@users.noreply.github.com>
2023-10-03 09:33:59 +0200

    Add files via upload

---
+++ progetto2_Caprarella/progetto2_Caprarelaa/src/App.java
@@ -0,0 +1,8 @@
+public class App {
+    public static void main(String[] args) throws Exception {
+        ServerStr servente = new ServerStr();
+        servente.attendi();
+        servente.comunica();
+
+    }
+}
